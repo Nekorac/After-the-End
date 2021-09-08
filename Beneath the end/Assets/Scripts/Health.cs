520a81136bbaa8f6188cfce4ba848fecd6d8368b
Axel <Axel.Thomsen.0446@student.uu.se>
2021-09-08 14:37:23 +0200

    Reorganized scene + implemented particles on death

--- Beneath the end/Assets/Scripts/Health.cs
+++ Beneath the end/Assets/Scripts/Health.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
     int health = 1;
+    bool dead = false;
+    public GameObject deathParticles;
 
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
+        if (health <= 0 && !dead)
         {
             Debug.Log("Dead");
+            dead = true;
             gameObject.SetActive(false);
+            Instantiate(deathParticles, gameObject.transform.position, deathParticles.transform.rotation);
+            Invoke("reloadScene", 5);
+
         }
+    }
+
+    void reloadScene()
+    {
+        SceneManager.LoadScene("Linda's scene");
     }
 
     public void removeHealth()
